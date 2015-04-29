5b6d2e205f417c50b5e70a87d081670dc09610ec
Nick Donias <pair+alycit.mweitzel.quackingduck@devbootcamp.com>
2015-04-29 08:11:47 -0400

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,49 @@ var router = express.Router();
 var appdata = require('../data.json');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+	var myCategory = [];
+	
+	myCategory = appdata.category;
+  	
+  	res.render('index', {
+  	title: 'Home',
+  	category: myCategory,
+  	page: 'home'
+	});
+});
+
+/* GET backpack page. */
+router.get('/backpack', function(req, res) {
+	var myCategory = [];
+	myCategory = appdata.category;
+
+  res.render('backpack', { 
+  	title: 'Backpack',
+  	category:  myCategory,
+  	page: 'backpackList'
+	});
+});
+
+/* GET backpack cards page. */
+router.get('/backpack/:backpackid', function(req, res) {
+	var myCategory = [];
+	var myFront = [];
+	var myBack = [];
+	appdata.backpack.forEach(function(item){
+		if (item.category == req.params.backpackid){
+			myCategory.push(item);
+			myFront = myCategory.concat(item.front);
+			myFront = myCategory.concat(item.back);
+		}
+	});
+  res.render('backpack', { 
+  	title: 'Backpack',
+  	category: myCategory,
+  	front: myFront,
+  	back: myBack,
+  	page: 'backpackDetail'
+	});
 });
 
 module.exports = router;
